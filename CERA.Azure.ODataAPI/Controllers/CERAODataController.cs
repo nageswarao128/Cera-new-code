e29e858e6314705d25ddabece85278804458d36d
Jitendriya Sendha <jitendriya.sendha@quadrantresource.com>
2021-02-05 08:45:48 +0530

    Removed Unwanted things

---
+++ CERA.Azure.ODataAPI/Controllers/CERAODataController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CERA.Azure.ODataAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CERAODataController : ControllerBase
+    {
+
+
+        private readonly ILogger<CERAODataController> _logger;
+
+        public CERAODataController(ILogger<CERAODataController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
