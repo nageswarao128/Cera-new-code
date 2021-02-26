6e8441b610a15412c4a6786ea93fe727e8583851
Jitendriya Sendha <jitendriya.sendha@quadrantresource.com>
2021-02-26 09:59:45 +0530

    Removed necesaary Code

--- TestCERAClient/Controllers/ManageController.cs
+++ TestCERAClient/Controllers/ManageController.cs
@@ -4,7 +4,9 @@ using CERAAPI.ViewModels;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 
 namespace CERAAPI.Controllers
 {
