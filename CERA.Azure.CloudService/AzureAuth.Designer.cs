7c1fa8822c24e897592c49082b9f4dc511cdaa26
sandeep.quadrant <karemsaisandeep@gmail.com>
2021-03-17 11:27:12 +0530

    Issues resolved

--- CERA.Azure.CloudService/AzureAuth.Designer.cs
+++ CERA.Azure.CloudService/AzureAuth.Designer.cs
@@ -12,7 +12,7 @@ namespace CERA.Azure.CloudService {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.7.0.0")]
     internal sealed partial class AzureAuth : global::System.Configuration.ApplicationSettingsBase {
         
         private static AzureAuth defaultInstance = ((AzureAuth)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new AzureAuth())));
